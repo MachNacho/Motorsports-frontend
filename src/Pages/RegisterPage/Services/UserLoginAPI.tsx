0576fa5f50ca962790f679fc961c797ee4912a1b
Asi <64479526+MachNacho@users.noreply.github.com>
2025-09-23 07:29:35 +0200

    remove unused token service

--- src/Pages/RegisterPage/Services/UserLoginAPI.tsx
+++ src/Pages/RegisterPage/Services/UserLoginAPI.tsx
@@ -2,12 +2,10 @@ import axios from "axios";
 import type { UserLogin } from "../Interfaces/UserLogin";
 import { loginUserEndpoint } from "../../../API/endpointlist";
 import type { UserReturn } from "../Interfaces/UserReturn";
-import { saveToken } from "../../../Services/TokenService";
 
 export const UserLoginAPI = async (user: UserLogin): Promise<UserReturn> => {
   const response = await axios.post(loginUserEndpoint, user);
   if (response.status === 200) {
-    saveToken(response.data.token); // Save the token to local storage
     return response.data;
   } else {
     throw new Error("Login failed");
